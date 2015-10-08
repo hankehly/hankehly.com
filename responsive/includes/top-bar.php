d31625e166e4fcfdd00ff2e754dc875a9b53431f
Hank Ehly <hank.ehly@mulodo.com>
2015-10-08 21:40:03 +0900

    remove stuff and add some changes

--- responsive/includes/top-bar.php
+++ responsive/includes/top-bar.php
@@ -3,5 +3,4 @@
   <section>
   	<a href="https://github.com/hehly" target="_blank">GitHub <span class="glyphicon glyphicon-new-window"></span></a>
   </section>
-</div><span class="glyphicon glyphicon-search" aria-hidden="true"></span>
 <?php include 'lineGradient.php'; ?>

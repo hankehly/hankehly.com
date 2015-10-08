655e1c90a7b56a0bf6c8fc56fda46ba60302a1fe
Hank Ehly <hank.ehly@mulodo.com>
2015-10-08 21:36:23 +0900

    add bunch of stuff to send to server

--- responsive/includes/header.php
+++ responsive/includes/header.php
@@ -6,6 +6,7 @@
   <title>hankehly.com</title>
   <meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
   <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE" />
+  <link rel="stylesheet" href="/bower_components/bootstrap/dist/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="responsive/CSS/stylesheet.css">
 </head>
 
--- responsive/includes/top-bar.php
+++ responsive/includes/top-bar.php
@@ -1,4 +1,7 @@
 <div id="headline">
   <header>hank<span>ehly</span>.com</header>
-</div>
+  <section>
+  	<a href="https://github.com/hehly" target="_blank">GitHub <span class="glyphicon glyphicon-new-window"></span></a>
+  </section>
+</div><span class="glyphicon glyphicon-search" aria-hidden="true"></span>
 <?php include 'lineGradient.php'; ?>

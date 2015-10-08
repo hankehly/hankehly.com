cbd74a9ca1e74cb78a39b6aebe1f13cd1adf60e6
Hank Ehly <hank.ehly@mulodo.com>
2015-10-08 21:44:50 +0900

    fix another syntax error

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -52,10 +52,10 @@
 server '160.16.63.151',
   user: 'henryehly',
   roles: %w{web app},
-  # ssh_options: {
+  ssh_options: {
   #   user: 'user_name', # overrides user setting above
   #   keys: %w(/home/user_name/.ssh/id_rsa),
   #   forward_agent: false,
   #   auth_methods: %w(publickey password)
     # password: 'please use keys'
-  # }
+  }

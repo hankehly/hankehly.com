383cd7bfa98c6a6d3d03103a8e49ee0bfa5393dd
Hank Ehly <hank.ehly@mulodo.com>
2015-11-18 10:39:37 +0900

    test

--- responsive/includes/header.php
+++ responsive/includes/header.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8">
-  <title>hankehly.com</title>
+  <title>foobar</title>
   <meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
   <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE" />
   <link rel="stylesheet" type="text/css" href="responsive/CSS/stylesheet.css">

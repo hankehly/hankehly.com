76362d7f858299c361e2283062d3af8c45036d44
Hank Ehly <henry.ehly@gmail.com>
2017-07-31 12:29:13 +0900

    Update og images

--- responsive/includes/header.php
+++ responsive/includes/header.php
@@ -5,13 +5,23 @@
   <meta charset="utf-8">
   <title>Hank Ehly Official Homepage</title>
   <meta name="description" content="Hank Ehly is a software developer at Mulodo Inc. in Tokyo, Japan. He has a passion for language learning and creative software design.">
+
+  <!-- twitter  -->
   <meta name="twitter:card" content="summary_large_image">
   <meta name="twitter:site" content="@hankehly">
   <meta name="twitter:creator" content="@hankehly">
   <meta name="twitter:title" content="Hank Ehly Official Homepage">
   <meta name="twitter:description" content="Hank Ehly is a software developer at Mulodo Inc. in Tokyo, Japan. He has a passion for language learning and creative software design.">
-  <meta name="twitter:image" content="https://hankehly.com/responsive/images/test-image-1.jpg">
+  <meta name="twitter:image" content="https://hankehly.com/responsive/images/meta/twitter_summary_large_image.png">
   <meta name="twitter:image:alt" content="Hank Ehly Official Homepage">
+
+  <!-- facebook -->
+  <meta property="og:url" content="https://hankehly.com">
+  <meta property="og:title" content="Hank Ehly Official Homepage">
+  <meta property="og:description" content="Hank Ehly is a software developer at Mulodo Inc. in Tokyo, Japan. He has a passion for language learning and creative software design.">
+  <meta property="og:image" content="https://hankehly.com/responsive/images/meta/og_image.png">
+  <meta property="og:type" content="website">
+
   <meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
   <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE" />
   <link rel="stylesheet" type="text/css" href="responsive/CSS/stylesheet.css">

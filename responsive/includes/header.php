3408a243d5f06da0a5f58c73fc4c0b9363eb5fb2
Hank Ehly <henry.ehly@gmail.com>
2017-07-31 12:09:06 +0900

    Add twitter meta tags

--- responsive/includes/header.php
+++ responsive/includes/header.php
@@ -5,6 +5,13 @@
   <meta charset="utf-8">
   <title>Hank Ehly Official Homepage</title>
   <meta name="description" content="Hank Ehly is a software developer at Mulodo Inc. in Tokyo, Japan. He has a passion for language learning and creative software design.">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@hankehly">
+  <meta name="twitter:creator" content="@hankehly">
+  <meta name="twitter:title" content="Hank Ehly Official Homepage">
+  <meta name="twitter:description" content="Hank Ehly is a software developer at Mulodo Inc. in Tokyo, Japan. He has a passion for language learning and creative software design.">
+  <meta name="twitter:image" content="https://hankehly.com/responsive/images/test-image-1.jpg">
+  <meta name="twitter:image:alt" content="Hank Ehly Official Homepage">
   <meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
   <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE" />
   <link rel="stylesheet" type="text/css" href="responsive/CSS/stylesheet.css">

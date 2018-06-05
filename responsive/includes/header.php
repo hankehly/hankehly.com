48f1f69156ade0aeead5c5fb522ec869a7e65c38
Hank Ehly <henry.ehly@gmail.com>
2018-06-05 18:50:47 +0900

    Update header.php

--- responsive/includes/header.php
+++ responsive/includes/header.php
@@ -29,6 +29,18 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   <meta property="og:description" content="Hank Ehly is a software developer at Mulodo Inc. in Tokyo, Japan. He has a passion for language learning and creative software design.">
   <meta property="og:image" content="https://hankehly.com/responsive/images/meta/og_image.png">
   <meta property="og:type" content="website">
+  
+  <!-- Hotjar Tracking Code for https://hankehly.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:905033,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 
   <meta name="viewport" content="initial-scale = 1.0,maximum-scale = 1.0" />
   <meta http-equiv="X-UA-Compatible" content="IE=9; IE=8; IE=7; IE=EDGE" />
